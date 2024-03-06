087c760188816e955a3196edf32c5e18f9ef834f
umangsthav2 <mangaleeshrestha@gmail.com>
2024-03-06 14:47:29 +0545

    Underdevelopment of Admin Dashboard

--- src/Pages/admin/Controllbar.jsx
+++ src/Pages/admin/Controllbar.jsx
@@ -4,28 +4,38 @@ function Controllbar() {
     return (
 
         <>
-            <div className="controlbar">
-                
-                <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Category
-                                </a>
-                                <ul className="dropdown-menu">
-                                    <li><a href='/category/fiction' className="dropdown-item">Fiction</a></li>
-                                    <li><a href='' className="dropdown-item">Horror</a></li>
-                                    <li><a href='' className="dropdown-item">Mystery</a></li>
-                                    <li><a href='' className="dropdown-item">Romance</a></li>
-                                    <li><a href='' className="dropdown-item">Thriller</a></li>
-                                    <li><hr className="dropdown-divider" /></li>
-                                    <li><a className="dropdown-item" href="#">Books Archive</a></li>
-                                </ul>
-                            </li>
+            <aside className="sidebar">
+                <div className="profile">
+                <img src="" alt="" />
+                    asjdkfl
+                </div>
 
-            </div>
+                    <a href=""><li>Navbar Options</li></a>
+                    <a href=""><li>Navbar Options</li></a>
+                    <a href=""><li>Navbar Options</li></a>
+                    <a href=""><li>Navbar Options</li></a>
+                    <a href=""><li>Navbar Options</li></a>
+                    <a href=""><li>Navbar Options</li></a>
+                    <a href=""><li>Navbar Options</li></a>
+                    <a href=""><li>Navbar Options</li></a>
+                    <a href=""><li>Navbar Options</li></a>
+                    <a href=""><li>Navbar Options</li></a>
+                    <a href=""><li>Navbar Options</li></a>
+                    <a href=""><li>Navbar Options</li></a>
+                    <a href=""><li>Navbar Options</li></a>
+                    <a href=""><li>Navbar Options</li></a>
+                    <a href=""><li>Navbar Options</li></a>
+            </aside>
+
+
+
+
+
+
+
+
 
         </>
-
-
 
     );
 }
